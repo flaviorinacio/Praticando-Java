9e429a634fea85fc7885f4b9813de734d12fe322
Flávio Inácio <flaviorinacio@outlook.com>
2022-10-04 20:49:00 -0300

    Subindo projeto incial de curso completo de java

---
+++ JavaCompleto/src/main/java/com/example/JavaCompleto/JavaCompletoApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaCompleto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCompletoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCompletoApplication.class, args);
+	}
+
+}
---
+++ JavaCompleto/src/test/java/com/example/JavaCompleto/JavaCompletoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaCompleto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCompletoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
